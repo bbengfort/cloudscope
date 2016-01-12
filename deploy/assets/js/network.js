88754e622b9b90e18f071d1c6ad13e347e934bb1
Benjamin Bengfort <benjamin@bengfort.com>
2016-01-11 21:06:48 -0500

    refactoring

---
+++ deploy/assets/js/network.js
@@ -0,0 +1,123 @@
+/*
+ * network.js
+ * Implements communications between replicas and animates them.
+ *
+ * Author:  Benjamin Bengfort <bengfort@cs.umd.edu>
+ * Created: Mon Jan 11 20:22:43 2016 -0500
+ *
+ * Dependencies:
+ *  - jquery
+ *  - underscore
+ */
+
+(function() {
+
+  // Message object to simulate network communication
+  Message = function(source, target, payload, options) {
+
+    this.source  = source;
+    this.target  = target;
+    this.payload = payload;
+    this.options = {
+      delay: 1000,
+      class: 'message'
+    };
+
+    // Sends the message
+    this.trigger = function(options) {
+
+      // Set default options
+      options = options || {};
+      this.options = _.defaults(options, this.options);
+      var self = this;
+
+      setTimeout(function() {
+        self.target.recv(self);
+      }, self.options.delay);
+
+      self.animate();
+
+      return self;
+    };
+
+    //  Animates a message circle
+    this.animate = function() {
+      var circle = $(utils.SVG('circle'))
+        .attr({'cx': this.source.layout.cx, 'cy': this.source.layout.cy, 'r': 8})
+        .attr('class', this.options.class)
+        .appendTo($("#network", this.source.sim.svg));
+
+      circle.velocity({
+        cx: this.target.layout.cx,
+        cy: this.target.layout.cy
+      }, {
+        duration: this.options.delay,
+        complete: function(elements) {
+          _.each(elements, function(element) {
+            $(element).remove();
+          });
+        }
+      });
+    }
+
+    // Clones a message with a new source and target
+    this.clone = function(source, target, options) {
+      options = options || {};
+      options = _.defaults(options, this.options)
+      return new Message(source, target, this.payload, options);
+    }
+
+    return this.trigger(options);
+  };
+
+  // Connection manages network properties as well as visual paths.
+  // Represents a unidirectional connection; bidirectional requires 2 connections
+  Connection = function(sim, source, target, link) {
+
+    this.sim     = null; // The parent simulation
+    this.source  = null; // The source (sending) replica
+    this.target  = null; // The target (destination) replica
+    this.type    = null; // The connection type (constant or variable)
+    this.latency = null; // Either a static integer or a min, max pair
+    this.path    = null; // The svg path of the connection (bezier curve)
+
+    this.init = function(sim, source, target, link) {
+      this.sim     = sim;
+      this.source  = source;
+      this.target  = target;
+      this.type    = link.connection || consts.CONSTANT;
+      this.latency = link.latency || config.default_latency;
+
+      // Return the connection object for use.
+      return this;
+    };
+
+    // Computes the latency on demand for the connection.
+    this.getLatency = function() {
+      if (this.type == consts.VARIABLE) {
+       var latency = random.randint(this.latency[0], this.latency[1]);
+       return latency * config.slowmo;
+      } else {
+       return this.latency * config.slowmo;
+      }
+    };
+
+    // Draw the bezier curve path onto the simulation.
+    this.draw = function(ring) {
+      ring = ring || this.sim.ringDimension();
+
+      var sp = "M" + this.source.layout.cx + "," + this.source.layout.cy;
+      var ep = this.target.layout.cx + "," + this.target.layout.cy;
+      var cp = "Q" + ring.cx + "," + ring.cy;
+
+      // Draw the edges onto the graph
+      this.path = $(utils.SVG("path"))
+        .attr("d", sp + " " + cp + " " + ep)
+        .attr("class", "link " + this.type)
+        .appendTo($("#network", this.sim.svg));
+    };
+
+    return this.init(sim, source, target, link);
+  };
+
+})();
--- deploy/assets/js/simulation.js
+++ deploy/assets/js/simulation.js
@@ -12,243 +12,13 @@
 
 (function() {
 
-  // Module Constants
-  var CONSTANT = "constant",
-      VARIABLE = "variable",
-      SLOWMO   = 3;  // Set this to 1 for real speed, or a factor < 10 for slow motion
-
-  // Module Variables
-  var sequence = new utils.Sequence();
-
-  // Message object to simulate network communication
-  Message = function(source, target, payload, options) {
-
-    this.source  = source;
-    this.target  = target;
-    this.payload = payload;
-    this.options = {
-      delay: 1000,
-      class: 'message'
-    };
-
-    // Sends the message
-    this.trigger = function(options) {
-
-      // Set default options
-      options = options || {};
-      this.options = _.defaults(options, this.options);
-      var self = this;
-
-      setTimeout(function() {
-        self.target.recv(self);
-      }, self.options.delay);
-
-      self.animate();
-
-      return self;
-    };
-
-    //  Animates a message circle
-    this.animate = function() {
-      var circle = $(utils.SVG('circle'))
-        .attr({'cx': this.source.layout.cx, 'cy': this.source.layout.cy, 'r': 8})
-        .attr('class', this.options.class)
-        .appendTo($("#network", this.source.env.svg));
-
-      circle.velocity({
-        cx: this.target.layout.cx,
-        cy: this.target.layout.cy
-      }, {
-        duration: this.options.delay,
-        complete: function(elements) {
-          _.each(elements, function(element) {
-            $(element).remove();
-          });
-        }
-      });
-    }
-
-    // Clones a message with a new source and target
-    this.clone = function(source, target, options) {
-      options = options || {};
-      options = _.defaults(options, this.options)
-      return new Message(source, target, this.payload, options);
-    }
-
-    return this.trigger(options);
-  };
-
-  // Replica object to simulate devices on the file system
-  Replica = function(env, options) {
-
-    this.id    = null;
-    this.env   = null;
-    this.svg   = null;
-    this.label = null;
-    this.type  = null;
-
-    this.consistency = null;
-    this.layout      = null;
-
-    this.comms = {};
-    this.files = [];
-
-    // Initializes a replica based on node data from a JSON graph.
-    this.init = function(env, node) {
-      this.id    = node.id;
-      this.env   = env
-      this.label = node.label || "Replica " + node.id;
-      this.type  = node.type || "storage";
-      this.files = node.files || [];
-      this.consistency = node.consistency || "strong";
-
-      return this;
-    };
-
-    // Sends a message to the destination
-    // For now message can be anything, dst must be a replica id.
-    this.send = function(msg, dst, options) {
-      var self = this;
-      var conn = self.comms[dst];
-
-      // Create a message with the version information
-      msg = msg || {};
-      msg = _.defaults(msg, {
-        creator: self,
-        version: sequence.next(),
-        level: self.consistency
-      });
-      self.files.push(msg);
-
-      // Add the message properties like network latency
-      options  = options || {};
-      options  = _.defaults(options, {
-        delay: conn.getLatency(),
-      });
-
-      return new Message(self, conn.target, msg, options);
-    };
-
-    // Handler for receiving a message
-    this.recv = function(msg) {
-      var self = this;
-
-      // "ACK" is currently the payload for an acknowledgement
-      if (msg.payload != "ACK") {
-        // Add the version to your files.
-        if (!_.contains(self.files, msg.payload)) {
-            self.files.push(msg.payload);
-
-            // Send updates to everyone else
-            _.each(this.comms, function(link) {
-              if (link.target != msg.source) {
-                return msg.clone(self, link.target, {delay: link.getLatency()});
-              }
-
-            });
-        }
-
-        // Send acknowledgment
-        options = {
-          delay: this.comms[msg.source.id].getLatency(),
-          class: 'response'
-        }
-        var response = new Message(this, msg.source, "ACK", options);
-        console.log(this.label + " received message from " + msg.source.label + " in " + msg.options.delay + " milliseconds.");
-
-      }
-    };
-
-    // Broadcast a message to all connected nodes
-    this.broadcast = function(msg) {
-      var self = this;
-      return _.map(self.comms, function(link) {
-        return self.send(msg, link.target.id);
-      });
-    };
-
-    // Adds connections based on link data from a JSON graph.
-    this.addConnection = function(link, replica) {
-      var connection = {
-        target: replica,        // replica destination
-        type: link.connection,  // constant or variable
-        latency: link.latency,  // either an integer or a min, max pair.
-        path: link.path,         // the path of the svg connection
-
-        // Computes the latency of the connection
-        getLatency: function() {
-          if (typeof(this.latency) == "number") {
-            return this.latency * SLOWMO;
-          } else {
-            return random.randint(this.latency[0], this.latency[1]) * SLOWMO;
-          }
-        }
-      }
-
-      this.comms[replica.id] = connection;
-      return connection;
-    };
-
-    // Draws the node to the parent environment
-    // Must take a layout parameter for the position of the node.
-    this.draw = function(layout) {
-      var self = this;
-      this.layout = layout;
-
-      // The node icon group
-      this.svg = $(utils.SVG('g'))
-        .attr("id", this.id)
-        .attr("class", "replica consistency-" + this.consistency)
-        .click(function() { self.broadcast(); })
-        .appendTo(this.env.svg);
-
-      // The node circle
-      $(utils.SVG('circle'))
-        .attr(layout)
-        .appendTo(this.svg);
-
-      // The icon circle
-      $(utils.SVG('circle'))
-        .attr({"cx": layout.cx, "cy": layout.cy, r: (layout.r * .75)})
-        .attr("filter", "url(#" + this.type + ")")
-        .appendTo(this.svg);
-
-      // The label of the node
-      var label = {
-        "text-anchor": "middle",
-        "x": layout.cx,
-        "class": "label"
-      }
-
-      // Move above if above the fold else move below
-      if (layout.cy < (this.env.height() / 2)) {
-        label.y = layout.cy - 8 - layout.r;
-      } else {
-        label.y = layout.cy + 18 + layout.r;
-      }
-
-      $(utils.SVG('text'))
-        .attr(label)
-        .text(this.label)
-        .appendTo(this.svg);
-    }
-
-    return this.init(env, options);
-  };
-
   Simulation = function(selector, url, options) {
 
     this.url     = null;
     this.svg     = null;
     this.elem    = null;
     this.nodes   = [];
-    this.margins = {
-      top: 38,
-      right: 10,
-      bottom: 38,
-      left: 10
-    };
-
+    this.margins = config.margins;
 
     // Initializes the simulation by loading data from a URL.
     this.init = function(selector, url, options) {
@@ -301,21 +71,13 @@
 
           // Add all the communiucation edges between replicas
           _.each(data.links, function(link) {
+            // Get node objects for the source and target
             var source = self.nodes[link.source];
             var target = self.nodes[link.target];
 
-            var sp = "M" + source.layout.cx + "," + source.layout.cy;
-            var ep = target.layout.cx + "," + target.layout.cy;
-            var cp = "Q" + ring.cx + "," + ring.cy;
-
-            // Draw the edges onto the graph
-            link.path = $(utils.SVG("path"))
-              .attr("d", sp + " " + cp + " " + ep)
-              .attr("class", "link " + link.connection)
-              .appendTo(network);
-
             // Add source and target connections (undirected graph).
-            source.addConnection(link, target);
+            // Note, only draw one of the connections, not both.
+            source.addConnection(link, target).draw(ring);
             target.addConnection(link, source);
           });
 
